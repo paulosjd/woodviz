d10f53df2291199feb93b1218ffa9dfca52a848a
Paul Davis <pjdavis@gmx.com>
2020-04-04 12:45:45 +0100

    Update various things

--- src/home/guest.js
+++ src/home/guest.js
@@ -4,47 +4,61 @@ import {Container, Row, Col, ListGroup, ListGroupItem} from 'reactstrap';
 import {setShowRegForm, focusUsernameInput} from "../store/actions/user";
 
 class GuestHome extends Component {
+    state = {
+        selectedItem: ''
+    };
+
+    setSelectedItem(name) {
+        this.setState({selectedItem: name})
+    }
 
     render() {
+        const intro = (
+            <React.Fragment>
+                <h4>Welcome to the Den</h4>
+                <p><span className='link-text' onClick={() => this.props.setShowReg(true)}>Register</span> {'or '}
+                    <span className='link-text' onClick={this.props.usernameFocus}> login</span>
+                </p>
+                <div className='bottom20'>
+                    <span className='link-text small-font' data-field='username' onClick={this.props.helpClickHandler}>
+                        Username reminder
+                    </span>{'. '}
+                    <span className='link-text small-font' data-field='password' onClick={this.props.helpClickHandler}>
+                        Password reset
+                    </span>.
+                </div>
+            </React.Fragment>
+        );
+        let listGroup;
+        if (!this.state.selectedItem) {
+            listGroup = (
+                <ListGroup className='menu-items-list'>
+                    <ListGroupItem>
+                        <h4 style={{textAlign: 'center'}}>Boards</h4>
+                    </ListGroupItem>
+                    <ListGroupItem onClick={() => this.setSelectedItem('sfo')} className='board-item' >
+                        <h4>Demo board</h4>
+                    </ListGroupItem>
+                </ListGroup>
+            )
+        }
+        console.log(this.state.selectedItem)
         return (
             <Container className='main-home'>
                 <Row>
                 <Col xs="4">
-                    <h4>Welcome to DenBeast App</h4>
-                    <p>
-                        <span className='link-text' onClick={() => this.props.setShowReg(true)}>
-                            Register
-                        </span> {'or '}
-                        <span className='link-text' onClick={this.props.usernameFocus}>
-                            login
-                        </span> {' and get stuck into some training'}
-                    </p>
-                    <div>
-                        <span className='link-text small-font' data-field='username'
-                              onClick={this.props.helpClickHandler}>
-                            Username reminder
-                        </span>{'. '}
-                        <span className='link-text small-font' data-field='password'
-                              onClick={this.props.helpClickHandler}>
-                            Password reset
-                        </span>.
-                    </div>
-                    <ListGroup className='menu-items-list'>
-                        <ListGroupItem className='hover-background'>
-                    <svg height="100" width="100">
-                        <circle className='mkrpl'  onClick={()=>console.log('sdfsdf')} cx="50" cy="50" r="40" stroke="black" fill="red" />
-                    </svg>
-                        </ListGroupItem>
-                    </ListGroup>
+                    { !this.state.selectedItem && intro }
+                    { this.state.selectedItem && (
+                        <button className='lg-back-btn' onClick={() => this.setSelectedItem('')} >Back</button>) }
+                    { listGroup }
                 </Col>
                 <Col xs="8">
-                    <img height={450} src='images/board.png' />
+                    <img height={450} src='images/demo_board.svg' />
                 </Col>
             </Row>
             </Container>
         );
     }
-
 }
 
 const mapStateToProps = ({registration}) => {
@@ -63,3 +77,9 @@ export default connect(
     mapStateToProps,
     mapDispatchToProps
 )(GuestHome);
+
+// (
+//     <svg height="20" width="20">
+//         <circle cx="5" cy="5" r="4" stroke="black" fill="red" />
+//     </svg>
+// )
