27e2258d5a3b88315be86630e46339eb894bda57
Paul Davis <pjdavis@gmx.com>
2020-10-25 00:29:32 +0100

    Styling and various minor amends to presentation logic

--- src/utils/general.js
+++ src/utils/general.js
@@ -40,6 +40,12 @@ export const getStars = rating => {
     }
 };
 
+export const compareNames = (a,b) => {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+};
+
 export const toTitleCase = (phrase) => {
     if (phrase.toUpperCase() === phrase) {
         return phrase
